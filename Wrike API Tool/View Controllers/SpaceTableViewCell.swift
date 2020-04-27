6c6b1896aa1fc4c8434d20f61489824e59964a33
Mark Tapia <mark.tapia@team.wrike.com>
2020-04-26 21:12:33 -0700

    feat: Support Spaces
    Add Spaces Table View Cell, VC and Xib

---
+++ Wrike API Tool/View Controllers/SpaceTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  SpaceTableViewCell.swift
+//  Wrike API Tool
+//
+//  Created by Mark Tapia on 4/25/20.
+//  Copyright © 2020 Mark Tapia. All rights reserved.
+//
+
+import UIKit
+
+class SpaceTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var spaceIconImage: UIImageView!
+    @IBOutlet weak var spaceTitleButton: StyledButton!
+    @IBOutlet weak var caretButton: StyledButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
