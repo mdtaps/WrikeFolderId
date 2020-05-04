3c223515d16c30a0098c863072c197409e0b0f68
Mark Tapia <mark.tapia@team.wrike.com>
2020-05-03 19:38:51 -0700

    feat: Support Spaces
    Add SpacesViewController and Xib

---
+++ Wrike API Tool/View Controllers/SpacesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SpacesViewController.swift
+//  Wrike API Tool
+//
+//  Created by Mark Tapia on 5/3/20.
+//  Copyright © 2020 Mark Tapia. All rights reserved.
+//
+
+import UIKit
+
+class SpacesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
